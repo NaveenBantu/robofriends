3dcc945d33067ba0e29c7c88e766b75467e62c88
Naveen Bantu <venkata-naveen.bantu@voith.com>
2021-11-06 13:05:32 +0100

    Remove default react app files - Add Card components for robohash api

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = (props) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${props.robot.id}?200x200`} alt="" />
+            <div>
+                <h2>{props.robot.name}</h2>
+                <p>{props.robot.email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
