1daa192bea1d2fc1b33c55aff611cea5ae58de98
Naveen Bantu <venkata-naveen.bantu@voith.com>
2021-11-08 23:29:12 +0100

    Fetch data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css'
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -21,13 +26,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1 className='f1'>Robo Friends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+
+        if (this.state.robots.length === 0) {
+            return <h1>LOADING</h1>
+        }
+        else {
+            return (
+                <div className="tc">
+                    <h1 className='f1'>Robo Friends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 }
 
